736ea25cc97473873c9772a6d628993b9690338e
Entreprenerd <valeranijr@gmail.com>
2021-04-19 20:03:43 +0200

    feat: completed CRUD for product

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -15,6 +15,13 @@ router.get("/", async (req, res) => {
   res.send(result);
 });
 
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const result = await db<Product>("products").where("id", id);
+  res.send(result);
+});
+
 router.post("/", async (req, res) => {
   const { name, price } = req.body;
   if (!name) {
@@ -27,6 +34,45 @@ router.post("/", async (req, res) => {
 
   const result = await db<Product>("products")
     .insert({ name, price })
+    .returning("*");
+
+  res.send(result);
+});
+
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, price } = req.body;
+  const toUpdate = {
+    name: "",
+    price: "",
+  };
+
+  if (!name && !price) {
+    return res.status(400).send("Please have either a name or a price change");
+  }
+
+  if (name) {
+    toUpdate.name = name;
+  }
+
+  if (price) {
+    toUpdate.price = price;
+  }
+
+  const updated = await db<Product>("products")
+    .where("id", "=", id)
+    .update(toUpdate)
+    .returning("*");
+
+  res.send(updated);
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const result = await db<Product>("products")
+    .where("id", "=", id)
+    .del()
     .returning("*");
 
   res.send(result);

c179f9e19a8a15cf4153efdaa48f2c171ea9a009
Entreprenerd <valeranijr@gmail.com>
2021-04-19 18:30:11 +0200

    4 Setup KnexFile

---
+++ knexfile.js
@@ -0,0 +1,34 @@
+/* eslint-disable */
+// Update with your config settings.
+require("dotenv").config()
+
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: {
+      connectionString: process.env.DATABASE_URL
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};

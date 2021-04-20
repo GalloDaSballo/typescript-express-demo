c092fa322c3faf5a93947b319bb7a3dd019e6f5e
Entreprenerd <valeranijr@gmail.com>
2021-04-20 16:42:22 +0200

    feat: passport local authentication

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,8 +1,6 @@
 import passport from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
-
 import crypto from "crypto";
-
 import db from "../db";
 
 passport.serializeUser((user: any, done) => {

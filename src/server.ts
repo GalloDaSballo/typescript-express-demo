25844b299d12f025c1cbc4e91907b6365e08b3c4
Entreprenerd <valeranijr@gmail.com>
2021-04-20 20:08:42 +0200

    feat: with CRA

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import session from "express-session";
+import path from "path";
 import routes from "./routes";
 import passport from "./auth";
 
 const app = express();
-const port = process.env.PORT || 3000;
+
+const clientBuildPath = path.join(`${process.cwd()}/client/build`);
+app.use(express.static(clientBuildPath));
+
+const port = process.env.PORT || 8080;
 app.use(express.json());
 
 app.use(
@@ -19,6 +24,10 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use("/api", routes);
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(clientBuildPath, "index.html"));
+});
 
 app.listen(port, () => {
   console.log(`App listening at port ${port}`);
